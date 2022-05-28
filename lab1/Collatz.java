a5decc934e2e1c3d11fdff7f4392d579c7b8498c
yufei8840 <xieyu8840@gmail.com>
2022-05-29 05:54:04 +0800

    a error of commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -10,7 +10,7 @@ public class Collatz {
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
 
@@ -24,20 +24,4 @@ public class Collatz {
         System.out.println();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
