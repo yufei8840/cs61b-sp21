283489fec2b2c47a1cf874aef1335b4f954bad5e
yufei8840 <xieyu8840@gmail.com>
2022-12-23 17:41:10 +0800

    rewrited lab4 partA

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -9,11 +9,30 @@ public class Flik {
      *  @return Whether a and b are the same */
     // 接收的参数改为int基本数据类型，或者是调用Integer的equals方法；
     // 错误在于传入参数是类对象，却进行了等号比较，实际上他们的内存地址不一样，输出是仍然是500,但是确实引用类型；
+
+
+
+
+
+
+
+
+
+
     public static boolean isSameNumber(int a, int b) {
         return a == b;
     }
 
-    public static boolean isSameNumber(Integer a, Integer b) {
-        return a.equals(b);
-    }
+
+
+
+
+
+
+
+
+
+//    public static boolean isSameNumber(Integer a, Integer b) {
+//        return a.equals(b);
+//    }
 }
--- lab4/flik/TestAll.java
+++ lab4/flik/TestAll.java
@@ -4,14 +4,31 @@ import org.junit.Test;
 
 public class TestAll {
 
-    @Test
-    public void test() throws Exception{
-        int i = 0;
-        for (int j = 0;i < 500;++i,++j) {
-            if (!Flik.isSameNumber(i,j)) {
-                System.out.println("i don't same with j.");
-            }
-        }
-        System.out.println("i is " + i);
-    }
+//    @Test
+//    public void test() throws Exception{
+//        int i = 0;
+//        for (int j = 0;i < 500;++i,++j) {
+//            if (!Flik.isSameNumber(i,j)) {
+//                System.out.println("i don't same with j.");
+//            }
+//        }
+//        System.out.println("i is " + i);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
